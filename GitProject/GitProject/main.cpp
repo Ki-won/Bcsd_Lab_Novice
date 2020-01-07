44dbdb5b0b2d5020ac6cadb254d0b43b8a548a31
Ki_won <seokw2240@gmail.com>
2020-01-07 21:15:58 +0900

    add pig class

--- GitProject/GitProject/main.cpp
+++ GitProject/GitProject/main.cpp
@@ -1,5 +1,32 @@
-int main(void) {
-	int a;
-	int b;
-	return 0;
+#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <string>
+using namespace std;
+// hello world;
+
+class Pig {
+private:
+	char Pig_name[20];
+	int Pig_weight;
+	
+public:
+	void getName( const char* name)
+	{
+		strcpy(Pig_name, name);
+		cout << Pig_name << endl;
+	}
+
+	void getWeight(int weight)
+	{
+		Pig_weight = weight;
+		cout << Pig_weight << "Kg" << endl;
+	}
+};
+
+int main(void)
+{
+	Pig a;
+
+	a.getName("hello_pig");
+	a.getWeight(200);
 }
