40fa0bdf625e1a942732d23ed2a19293293fdb59
jin-YSH <jeonjin0704@gmail.com>
2020-01-08 21:54:34 +0900

    토끼 클래스 추가

---
+++ GitProject/GitProject/토끼.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNING
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Rabbit
+{
+private:
+		string rabbit_name;
+		double rabbit_weight;
+public:
+	void set_name(string name)
+	{
+		rabbit_name= name;
+	}
+	void set_weight(double wei)
+	{
+		rabbit_weight = wei;
+	}
+	void get_name()
+	{
+		std::cout << rabbit_name << std::endl;
+	}
+	void get_weight()
+	{
+		std::cout << rabbit_weight << std::endl;
+	}
+
+};
+int main()
+{
+    
+}
