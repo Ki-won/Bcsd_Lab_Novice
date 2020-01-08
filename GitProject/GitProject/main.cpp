6de92c1944d490ff26e32c2570cfbb32735bf276
hwanhui <hwanhee2962@gamil.com>
2020-01-08 22:02:57 +0900

    Revert "Revert "add cat""
    This reverts commit 7131c4a577908ad2072cf92313c2822453281adc.

--- GitProject/GitProject/main.cpp
+++ GitProject/GitProject/main.cpp
@@ -1,4 +1,26 @@
-int main(void) {
-	int a;
-	return 0;
-}
+#include <iostream>
+#include <string>
+
+class cat
+{
+public:
+	cat(std::string cat_Name, double cat_Weight)
+	{
+		this->cat_Name = cat_Name;
+		this->cat_Weight = cat_Weight;
+	}
+
+	void get_Name()
+	{
+		std::cout << cat_Name << std::endl;
+	}
+
+	void get_Weight()
+	{
+		std::cout << cat_Weight << std::endl;
+	}
+
+private:
+	std::string cat_Name;
+	double cat_Weight;
+};
